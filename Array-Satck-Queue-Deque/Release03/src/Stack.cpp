d4cf65183041ebf44a6287ba0c0eebaff9b2ce95
Hend Emad <32018228@hti.edu.eg>
2024-10-03 00:35:18 +0300

    Stack cpp file

---
+++ Array-Satck-Queue-Deque/Release03/src/Stack.cpp
@@ -0,0 +1,35 @@
+#include "Stack.h"
+
+Stack::Stack(int init_capacity) : Array(init_capacity) {}
+
+void Stack::add_element(int element) {
+    Array::add_element(element); 
+}
+
+void Stack::print() const {
+    Array::print();
+}
+
+int Stack::remove_element(){
+    if (is_empty()) 
+        throw out_of_range("Stack is empty!\n");
+    
+    int value = arr_data[size - 1];  
+    size--;  
+    shrink_array();  
+    return value;
+}
+
+int Stack::peek() {
+    if (is_empty()) 
+        throw out_of_range("Stack is empty!\n");
+
+    return arr_data[size - 1]; 
+}
+
+void Stack::process_elements(const function<void(int)>& process_fn) {
+    while(!is_empty()) {
+        int element = Stack::remove_element();
+        process_fn(element);
+    }
+}
