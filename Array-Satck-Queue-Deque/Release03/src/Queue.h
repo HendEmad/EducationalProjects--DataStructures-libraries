47c6f8f56476740b1d80839c6b1fc0fa13b5135f
Hend Emad <32018228@hti.edu.eg>
2024-10-03 00:37:54 +0300

    Queue header

---
+++ Array-Satck-Queue-Deque/Release03/src/Queue.h
@@ -0,0 +1,19 @@
+// src/Queue.h
+#ifndef QUEUE_H
+#define QUEUE_H
+
+#include "Array.h"
+
+class Queue : public Array {
+    int front, rear;
+
+public:
+    void print() const override;
+    Queue(int init_capacity = 10, int front = 0, int rear = 0);
+    void add_element(int element) override;
+    int remove_element() override;
+    int get_front();
+    void process_elements(const function<void(int)>& provess_fn) override;
+};
+
+#endif // QUEUE_H
